49d9c39f22c7d647b8e3b21452ad6d5e85ecd16b
Feng Wu <2000013727@stu.pku.edu.cn>
2022-03-22 16:16:05 +0800

    "World parts: hallway and room. just skeleton"

---
+++ proj2/byog/Core/WorldParts.java
@@ -0,0 +1,34 @@
+package byog.Core;
+
+import byog.TileEngine.TETile;
+
+/** Parts(hallways and rooms) consisting of the world. */
+public class WorldParts {
+    /** #### */
+    public static void straightHallWay(Position p, int length, TETile t) {  // 0
+
+    }
+    /** #####
+     *      #
+     *      #
+     */
+    public static void turnDownHallWay(Position p,int strightLen,int downLen, TETile t) { // 1
+
+    }
+    /**       #
+     *        #
+     *        #
+     *   ######
+     */
+    public static void turnUpHallWay(Position p, int strightLen,int upLen,TETile t) {  // 2
+
+    }
+    /**  #### ###
+     *   #      #
+     *   #      #
+     *   ########
+     */
+    public static void room(Position p,int width,int hight,TETile t) {  // 3
+
+    }
+}
