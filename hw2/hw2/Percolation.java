b6783849e03204e749b0ca5be2b1ad3dd843001a
Feng Wu <2000013727@stu.pku.edu.cn>
2022-04-09 17:14:48 +0800

    "Percolation.java completed using UF"

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,77 +3,85 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import java.lang.*;
 
+
+
 public class Percolation {
-    private int maxRow;
-    private int maxCol;
-    private int[][] sites;  // -1: blocked, 0: empty open, 1: full open
-    /** Creates N-by-N grid, with all sites initially blocked. */
+    private int head=0;
+    private int tail;
+    private int len;
+    private boolean[] open;
+    private int openSitesNum=0;
+    private WeightedQuickUnionUF uf;
     public Percolation(int N) {
         if(N<0) {
-            throw new IllegalArgumentException("Illegal: N<0");
+            throw new IllegalArgumentException();
         }
-        sites=new int[N][N];
-        maxCol=maxRow=N-1;
+        len=N;
+        tail=N*N+1;
+        open=new boolean[N*N+2];  // default 0
+        uf=new WeightedQuickUnionUF(N*N+2); // one head and one tail
+        for(int j=0;j<len;j++) {
+            int code1=xyTo1D(0,j);
+            uf.union(head,code1);  // union the top level with head
+            int code2=xyTo1D(len-1.j);
+            uf.union(code2,tail);  // union the bottom level with tail
+        }
     }
-    /** Returns true if (row, col) is out of range. */
     private boolean outOfRange(int row, int col) {
-        if(row<0 || row>maxRow) {
-            return false;
-        }else if(col<0 || col>maxCol) {
-            return false;
-        }else {
+        if(row<0 ||row>=len) {
             return true;
+        }else if(col<0 ||col>=len) {
+            return true;
+        }else {
+            return false;
         }
+    }
+    private int xyTo1D(int row, int col) {
+        return (row*len)+(col)+1;  // starts at 1 to N*N
     }
 
-    /** Opends the site (row, col) if it's not open already. */
     public void open(int row, int col) {
-        if(outOfRange(row,col)) {
-            throw new IndexOutOfBoundsException("(row, col) is out of range");
+        if(outOfRange(row, col)) {
+            throw new IndexOutOfBoundsException();
         }
-        sites[row][col]=0;
+        int code=xyTo1D(row,col);
+        open[code]=true;
+        openSitesNum++;
+        // dealing with neighbors.
+        if(!outOfRange(row-1,col)&&isOpen(row-1,col)) {
+            int upNeighbor=xyTo1D(row-1,col);
+            uf.union(upNeighbor,code);
+        }
+        if(!outOfRange(row,col-1)&&isOpen(row,col-1)) {
+            int leftNeighbor=xyTo1D(row,col-1);
+            uf.union(leftNeighbor,code);
+        }
+        if(!outOfRange(row,col+1)&&isOpen(row,col+1)) {
+            int rightNeighbor=xyTo1D(row,col+1);
+            uf.union(rightNeighbor,code);
+        }
+        if(!outOfRange(row+1,col)&&isOpen(row+1,col)) {
+            int downNeighbor=xyTo1D(row+1,col);
+            uf.union(downNeighbor,code);
+        }
+
     }
-    /** is the site (row, col) open? */
     public boolean isOpen(int row, int col) {
         if(outOfRange(row, col)) {
             throw new IndexOutOfBoundsException();
         }
-        if(sites[row][col]==-1) {
-            return false;
-        }else {
-            return true;
-        }
+        int code=xyTo1D(row,col);
+        return open[code];
     }
-    /** is the site (row, col) full? */
     public boolean isFull(int row, int col) {
-        if(outOfRange(row, col)) {
-            throw new IndexOutOfBoundsException();
-        }
-        if(sites[row][col]==1) {
-            return true;
-        }else {
-            return false;
-        }
+        int code=xyTo1D(row,col);
+        return uf.connected(code,head);
     }
-    /** Number of open sites. */
     public int numberOfOpenSites() {
-        int noos=0;
-        for(int i=0;i<=maxRow;i++) {
-            for(int j=0;j<=maxCol;j++) {
-                if(isOpen(i,j)) {
-                    noos++;
-                }
-            }
-        }
-        return noos;
+        return openSitesNum;
     }
-    /** Does the system percolate? */
     public boolean percolates() {
-        for(int j=0;j<maxCol;j++) {
-            if(isFull(maxRow,j)) {
-                return true;
-            }
-        }
-        return false;
+        return uf.connected(head,tail);
     }
+
 }
